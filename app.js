c6f68c2bc13128e01544e8f55cc3f9fa179ca979
porkunran <porkunran@gmail.com>
2021-03-29 23:10:13 +0530

    push try 3

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post("/", (req, res) => {
   webpush.setVapidDetails("mailto:admin@ponnmaravathy.in", publicKey, privateKey);
   webpush.sendNotification(sub, JSON.stringify(payLoad));
 
-  // res.send(sub);
+  res.statusCode(200);
 });
 // app.get("/", (req, res) => {
 //   var sub = req;
