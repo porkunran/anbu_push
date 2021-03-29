4016165e4b848ee9acab7f59ef83c9f3e2d7c317
porkunran <porkunran@gmail.com>
2021-03-29 22:59:46 +0530

    push try 1

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ const express = require("express");
 const app = express();
 const webpush = require("web-push");
 var mysql = require("mysql");
-const cors = require('cors');
+const cors = require("cors");
 const corsOpts = {
-  origin: '*',
+  origin: "*",
 
-  methods: [
-    'GET',
-    'POST',
-  ],
+  methods: ["GET", "POST"],
 
-  allowedHeaders: [
-    'Content-Type',
-  ],
+  allowedHeaders: ["Content-Type"],
 };
 app.use(cors(corsOpts));
 
@@ -28,19 +23,20 @@ app.use(cors(corsOpts));
 //     if (err) throw err;
 //     console.log("Connected!");
 //   });
-const publicKey = 'BMm30CHLv6QuTWn-JIUtSUzl_FtUjGaWDFC_tQDAYcfiFyaL8OJ6Z_JHRrS3gBnJ1jlGbmlEAg6YkDVWukTFl_M';
-const privateKey = 'TUBVedLao6KSoA2EOnSh7pE-blR68Nnh6QmIxkQo4G4';
-const payLoad ={
-notification:{
-data: {url: 'https://ponnamaravathy.in/'},
-title: 'Porkuran',
-vibrate: [100,50,100]
-}
+const publicKey =
+  "BMm30CHLv6QuTWn-JIUtSUzl_FtUjGaWDFC_tQDAYcfiFyaL8OJ6Z_JHRrS3gBnJ1jlGbmlEAg6YkDVWukTFl_M";
+const privateKey = "TUBVedLao6KSoA2EOnSh7pE-blR68Nnh6QmIxkQo4G4";
+const payLoad = {
+  notification: {
+    data: { url: "https://ponnamaravathy.in/" },
+    title: "Porkuran",
+    vibrate: [100, 50, 100],
+  }
 };
 app.post("/", (req, res) => {
-    var sub = req;
-    webpush.setVapidDetails('admin@ponnmaravathy.in', publicKey, privateKey)
-    webpush.sendNotification(sub, JSON.stringify(payLoad));
+  var sub = req;
+  webpush.setVapidDetails("admin@ponnmaravathy.in", publicKey, privateKey);
+  webpush.sendNotification(sub, JSON.stringify(payLoad));
 
   // res.send(sub);
 });
